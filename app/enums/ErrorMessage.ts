d40fc1b12d3b698743b4bcd37dc20de7ad428a50
laithha <147397351+laithha@users.noreply.github.com>
2025-05-22 14:19:43 +0300

    .

---
+++ app/enums/ErrorMessage.ts
@@ -0,0 +1,7 @@
+export enum ErrorMessages {
+    FETCH_BACKPACK = "Error fetching backpack",
+    QUERY_DB_SELECT = "Error querying database for select operation",
+    QUERY_DB_INSERT = "Error querying database for select operation",
+    QUERY_DB_UPDATE = "Error querying database for select operation",
+    QUERY_DB_DELETE = "Error querying database for select operation"
+}
